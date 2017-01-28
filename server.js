4bd79604acd682f7f494abf564d566a86b4d6786
ptlergo <ptlergo@gmail.com>
2017-01-28 05:20:42 -0500

    server update

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       morgan = require('morgan'),
       path = require('path');
+
+app.use(bodyParser/urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use( (req, res, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authrization');
+  next();
+
+});
