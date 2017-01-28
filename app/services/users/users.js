d2774070db1bcaec6333e970139631fff24a9bbe
ptlergo <ptlergo@gmail.com>
2017-01-28 16:39:54 -0500

    userList in users file

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -1,11 +1,37 @@
 // Create module and factory of Users
 angular.module('api.users', []).
   factory('Users', () => {
+
     const Users = {};
+
+    // Array of users to ttest factory
+    const userList = [
+      {
+        id: '0',
+        name: 'Jane',
+        role: 'Designer',
+        location: 'New York',
+        twitter: 'gijane'
+      },
+      {
+        id: '1',
+        name: 'Bob',
+        role: 'Developer',
+        locationL 'New York',
+        twitter: 'billybob'
+      },
+      {
+        id: '2',
+        name: 'Jim',
+        role: 'Developer',
+        location: 'Chicago',
+        twitter: 'jimbo'
+      }
+    ];
 
     // Define all to pass test
     Users.all = () => {
-
+      return userList;
     };
 
     return Users;
