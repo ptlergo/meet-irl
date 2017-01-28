0883e6a35e6a1a1ee750eb1cf0b4674d3ef65f07
ptlergo <ptlergo@gmail.com>
2017-01-28 15:13:17 -0500

    karma specs built for users factory testing

--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,7 +1,18 @@
 describe('Users factory', function() {
 
-  it('has a dummy spec to test 2 + 2', function() {
-    expect().toEqual(4);
+  let Users;
+
+  // Before each test load our api.users module
+  beforeEach(angular.mock.module('api.users'));
+
+  // Before each test set our injected Users factory (_Users_) to our local Users variable
+  beforeEach(inject(function(_Users_) {
+    Users = _Users_;
+  }));
+
+  // A simple test to verify the Users factory exists
+  it('should exist', function() {
+    expect(Users).toBeDefined();
   });
-  
+
 });
